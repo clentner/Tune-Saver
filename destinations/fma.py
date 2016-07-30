0a81fa19791c96e49251a35494e457ec6725f417
Chris Lentner <cjl8505@rit.edu>
2016-07-30 17:56:48 -0400

    Refactor Free Music Archive saving.
    Split out searching from saving.
    Use requests params instead of hand-rolling GET URL.

--- destinations/fma.py
+++ destinations/fma.py
@@ -3,6 +3,9 @@ import webbrowser
 
 from destinations.destination import Destination
 
+correct_track_prompt = '''
+Opened your browser to a track download link.
+Was it the correct song? y/n '''
 
 class FMA(Destination):
     name = "Free Music Archive"
@@ -10,6 +13,27 @@ class FMA(Destination):
     def __init__(self, config):
         self.config = config
 
+    def search(self, track):
+        '''
+        Search the Free Music Archive for the track.
+        @param track A pylast track object
+        @return the track ID (str), or None if not found.
+        '''
+        searchURL = 'https://freemusicarchive.org/api/trackSearch'
+        q = track.artist.name + ' - ' + track.title
+        params = {'q': q, 'limit': '1'}
+        r = requests.get(searchURL, params=params)
+        response = r.json()
+        
+        desc = response["aRows"][0]
+        # Extract track ID from parenthetical at end of string
+        id = desc[1 + desc.rfind('('):-1]
+        
+        # Kurt Vile's song Freeway is returned for all unsuccessful searches.
+        if id == '10' and (track.artist.name != "Kurt Vile" or track.title != "Freeway"):
+            return None        
+        return id
+        
     def save(self, track):
         '''
         @param track A pylast track object
@@ -17,22 +41,14 @@ class FMA(Destination):
         '''
         # Search for the track using the API
         q = track.artist.name + ' - ' + track.title
-        searchURL = 'https://freemusicarchive.org/api/trackSearch?q=' + q + '&limit=1'
-        r = requests.get(searchURL)
-        response = r.json()
-        desc = response["aRows"][0]
-        # Extract track ID from parenthetical at end of string
-        id = desc[1 + desc.rfind('('):-1]
-
-        # Kurt Vile's song Freeway is returned for all unsuccessful searches
-        # (?)
-        if id == '10' and (track.artist.name !=
-                           "Kurt Vile" or track.title != "Freeway"):
+        id = self.search(track)
+        if not id:
             return (False, "Free Music Archive search did not find {}".format(q))
 
         # Get track's webpage URL
-        track_endpoint = 'https://freemusicarchive.org/api/get/tracks.json?track_id={}&limit=1'
-        r = requests.get(track_endpoint.format(id))
+        track_endpoint = 'https://freemusicarchive.org/api/get/tracks.json'
+        params = {'track_id': id, 'limit': '1'}
+        r = requests.get(track_endpoint, params=params)
         response = r.json()
         track_url = response["dataset"][0]["track_url"]
 
@@ -48,7 +64,6 @@ class FMA(Destination):
         webbrowser.open(download_link)
 
         # Ask the user if the file was correct (FMA's search is pretty bad)
-        if 'y' == input('Was that the correct song? y/n '):
-            return (True, 'Opened Free Music Archive download link.')
-        else:
+        if 'y' != input(correct_track_prompt):
             return (False, 'Free Music Archive search found incorrect track.')
+        return (True, 'Opened Free Music Archive download link.')
