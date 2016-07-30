2f47138f69f80cce70de3e3f38630225a0deee42
Chris Lentner <cjl8505@rit.edu>
2016-07-30 17:40:03 -0400

    Refactor Jamendo implementation.
    Separate out the search() method from the save() part.
    Use requests params option instead of hand-rolling the GET URL.
    Document behavior in the config template.

--- destinations/jamendo.py
+++ destinations/jamendo.py
@@ -11,33 +11,45 @@ class Jamendo(Destination):
     def __init__(self, config):
         self.config = config
 
+    def search(self, track):
+        '''
+        @param track A pylast track object
+        @return The download URL, if the track can be found. None otherwise.
+        '''
+        endpoint = 'https://api.jamendo.com/v3.0/tracks/'
+        params = {
+            'format': 'json',
+            'limit': '1',
+            'client_id': self.config['client_id'],
+            'artist_name': track.artist.name,
+            'name': track.title
+        }
+        r = requests.get(endpoint, params=params)
+        response = r.json()
+        if response['headers']['results_count'] < 1:
+            # track not found
+            return None
+        return response['results'][0]['audiodownload']
+        
     def save(self, track):
         '''
         @param track A pylast track object
         @return True iff saving was successful
         '''
-        self.name = 'Jamendo'
-        endpoint = 'https://api.jamendo.com/v3.0/tracks/?format=json&limit=1&client_id=' + \
-            self.config['client_id']
-        q = '&artist_name=' + track.artist.name + '&name=' + track.title
-        url = endpoint + q
-        r = requests.get(url)
-        response = r.json()
-        if response['headers']['results_count'] < 1:
-            # track not found
-            return (False, "Jamendo search returned no results for {}".format(q))
-
-        download_url = response['results'][0]['audiodownload']
+        download_url = self.search(track)
+        if not download_url:
+            return (False, "Jamendo search returned no results")
+        
         if 'save_directory' in self.config:
             # download the song directly to the specified location
-            filename = os.path.join(self.config['save_directory'],
-                                    '{} - {}.mp3'.format(track.artist.name, track.title))
+            filename = '{} - {}.mp3'.format(track.artist.name, track.title)
+            filepath = os.path.join(self.config['save_directory'], filename)
             r = requests.get(download_url, stream=True)
             if r.status_code == 200:
-                with open(filename, 'wb') as f:
-                    r.raw.decode_content = True
-                    shutil.copyfileobj(r.raw, f)
-                return (True, "Saved from Jamendo to {}".format(filename))
+                with open(filepath, 'wb') as f:
+                    r.raw.decode_content = True   # Uncompress gzipped content
+                    shutil.copyfileobj(r.raw, f)  # Save to disk
+                return (True, "Saved from Jamendo to {}".format(filepath))
             else:
                 return (False, "Jamendo download URL returned status {}".format(r.status_code))
         else:
