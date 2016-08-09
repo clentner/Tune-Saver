ba4d4f9baa24816aaa9f653f182f320337144a68
Chris Lentner <cjl8505@rit.edu>
2016-08-09 13:21:43 -0400

    Document HTML scraping

--- services/last_fm_purchase.py
+++ services/last_fm_purchase.py
@@ -21,23 +21,31 @@ class LastFmPurchase(Service):
         @param track A pylast track object
         @return A list containing up to one ServiceTrack
         '''
+        # Download the Last.fm webpage for this track.
         r = requests.get(track.get_url())
+        # Parse out the purchase link tags, <a data-analytics-action="Buy|Direct">
         soup = BeautifulSoup(r.text, "html.parser")
         buy_links = soup.find_all('a', attrs={'data-analytics-action': 'Buy|Direct'})
+        # Some tracks do not have this information.
         if len(buy_links) == 0:
             return []
+        # Extrack the URL from the <a> tag
         buy_link = buy_links[0].get('href')
+        # The price will be in the text of the tag. Strip surrounding whitespace.
         price = buy_links[0].get_text().strip()
+        # Match out the price, to remove extraneous text.
         try:
             price = re.search(r'(\$\d*\.\d+)', price).group(0)
         except AttributeError:
-            pass
+            pass  # Better to have extra text than no price.
+        # Prompt to be displayed to user
         st = ServiceTrack('Buy "{} - {}" from {} for {}'.format(
             track.artist.name,
             track.title,
             urlparse(buy_link).hostname,
             price))
-        st.buy_link, st.price = buy_link, price
+        # Save the link to be opened if the user chooses this option.
+        st.buy_link = buy_link
         return [st]
     
     def save(self, servicetrack):
