003759e8d460cc1933d190b12eb8075736d39ba3
Chris Lentner <cjl8505@rit.edu>
2016-08-09 13:28:21 -0400

    Add some clarifying inline comments to soundcloud_download

--- services/soundcloud_download.py
+++ services/soundcloud_download.py
@@ -22,14 +22,19 @@ class SoundcloudDownload(soundcloud_service.Soundcloud):
         @param track A pylast track object
         @return A list containing ServiceTrack objects
         '''
+        # search_first is inherited from soundcloud_service.Soundcloud
         sc_track = self.search_first(track)
         if not sc_track or not sc_track.downloadable:
             return []
 
+        # Pass prompt string back to UI
         st = ServiceTrack('Download "{}" to {}'.format(
             sc_track.title,
             self.config['save_directory']))
+        # Save the track for downloading,
         st.track = sc_track
+        # but use Last.fm's metadata because
+        # the SoundCloud title is often unreliable.
         st.artist = track.artist.name
         st.title = track.title
         return [st]
@@ -44,6 +49,8 @@ class SoundcloudDownload(soundcloud_service.Soundcloud):
         sc_track = servicetrack.track
         download_url = sc_track.download_url + \
             '?client_id=' + soundcloud_service.client_id
+        # Download track to config['save_directory']
+        # using service.Service.download utility method
         filename = self.download(
             download_url,
             servicetrack.artist,
